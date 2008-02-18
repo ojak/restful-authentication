0cdce9a640f6af8a8ca3090781d827e370e62b07
Daniel Neighman <has.sox@gmail.com>
2008-02-19 03:05:55 +1100

    Gets the rspec generators to work
    Can't vouch for the state of the specs

--- rspec_generators/merbful_authentication_tests/merbful_authentication_tests_generator.rb
+++ rspec_generators/merbful_authentication_tests/merbful_authentication_tests_generator.rb
@@ -1,5 +1,4 @@
-class MerbfulAuthenticationTestsGenerator < RubiGen::Base
-  
+class MerbfulAuthenticationTestsGenerator < Merb::GeneratorBase
   attr_reader   :name,  
                 :class_name, 
                 :class_path, 
@@ -27,30 +26,65 @@ class MerbfulAuthenticationTestsGenerator < RubiGen::Base
                 :include_activation
   
   def initialize(runtime_args, runtime_options = {})
+    @base = File.dirname(__FILE__)
     super
     usage if args.empty?
     @name = args.shift
     extract_options
-    runtime_options.each{ |k,v| self.instance_variable_set("@#{k}", v) }    
+    runtime_options.each do |k,v| 
+      ivar_name = "@#{k}"
+      self.instance_variable_set("@#{k}", v) unless ivar_name.include?("/")  
+    end
   end
 
   def manifest
     record do |m|
-      # Ensure appropriate folder(s) exists
-      m.directory "spec"
-      m.directory "spec/models"
-      m.directory "spec/controllers"
-      m.directory "spec/mailers"
-      # Create stubs
-      m.template "model_spec_helper.rb", File.join("spec",  "#{file_name}_spec_helper.rb")
-      m.template "authenticated_system_spec_helper.rb", File.join("spec", "authenticated_system_spec_helper.rb")
-      m.template "session_controller_spec.rb", File.join("spec/controllers", "#{controller_file_path}_spec.rb")
-      m.template "model_controller_spec.rb", File.join("spec/controllers", "#{model_controller_file_path}_spec.rb")
-      m.template "model_spec.rb", File.join('spec/models', "#{file_name}_spec.rb")     
       
-      if include_activation
-        m.template "user_mailer_spec.rb", File.join("spec/mailers", "#{singular_name}_mailer_spec.rb")
+      @m = m
+      # Ensure appropriate folder(s) exists
+      @assigns = {
+        :name                                 => name,  
+        :class_name                           => class_name, 
+        :class_path                           => class_path, 
+        :file_name                            => file_name, 
+        :class_nesting                        => class_nesting, 
+        :class_nesting_depth                  => class_nesting_depth, 
+        :plural_name                          => plural_name, 
+        :singular_name                        => singular_name,
+        :controller_name                      => controller_name,
+        :controller_class_path                => controller_class_path,
+        :controller_file_path                 => controller_file_path,
+        :controller_class_nesting             => controller_class_nesting,
+        :controller_class_nesting_depth       => controller_class_nesting_depth,
+        :controller_class_name                => controller_class_name,
+        :controller_singular_name             => controller_singular_name,
+        :controller_plural_name               => controller_plural_name,
+        :model_controller_name                => model_controller_name,
+        :model_controller_class_path          => model_controller_class_path,
+        :model_controller_file_path           => model_controller_file_path,
+        :model_controller_class_nesting       => model_controller_class_nesting,
+        :model_controller_class_nesting_depth => model_controller_class_nesting_depth,
+        :model_controller_class_name          => model_controller_class_name,
+        :model_controller_singular_name       => model_controller_singular_name,
+        :model_controller_plural_name         => model_controller_plural_name,
+        :include_activation                   => include_activation
+      }
+      
+      @choices = []
+      @choices << "spec/mailers"
+      Dir[File.join(@base, "spec", "mailers")].each do |f|
+        @choices << relative(f)
       end
+      
+      @choices.each do |f|
+        options[f] = include_activation ? true : false
+      end
+      
+      m.directory( File.join("spec", "controllers", controller_class_path))
+      m.directory( File.join("spec", "controllers", model_controller_class_path))
+
+      copy_dirs
+      copy_files
     end
   end
 
