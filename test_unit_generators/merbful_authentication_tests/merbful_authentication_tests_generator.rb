1e8496cf1b2c769b141d690484ac6c04d077f80f
Daniel Neighman <has.sox@gmail.com>
2008-02-19 03:34:02 +1100

    Gets the test_unit generators working

--- test_unit_generators/merbful_authentication_tests/merbful_authentication_tests_generator.rb
+++ test_unit_generators/merbful_authentication_tests/merbful_authentication_tests_generator.rb
@@ -31,27 +31,55 @@ class MerbfulAuthenticationTestsGenerator < Merb::GeneratorBase
     super
     @name = args.shift
     extract_options
-    runtime_options.each{ |k,v| self.instance_variable_set("@#{k}", v) }    
+    runtime_options.each do |k,v| 
+      ivar_name = "@#{k}"
+      self.instance_variable_set("@#{k}", v) unless ivar_name.include?("/")  
+    end  
   end
 
   def manifest
     record do |m|
-      # Ensure appropriate folder(s) exists
-      m.directory "test"
-      m.directory "test/unit"
-      m.directory "test/functional"
+      @m = m
       
-      m.directory "test/mailers" if include_activation
-      # Create stubs
-      m.template "model_test_helper.rb", File.join("test",  "#{file_name}_test_helper.rb")
-      m.template "authenticated_system_test_helper.rb", File.join("test", "authenticated_system_test_helper.rb")
-      m.template "model_functional_test.rb", File.join("test", "functional", "#{model_controller_file_path}_test.rb")
-      m.template "functional_test.rb", File.join("test", "functional", "#{controller_file_path}_test.rb")
-      m.template "unit_test.rb", File.join("test", "unit", "#{singular_name}_test.rb")
-    
-      if include_activation
-        m.template "mailer_test.rb", File.join("test/mailers", "#{singular_name}_mailer_test.rb")
-      end
+      @choices = []
+      @choices << relative(File.join(@base, "test", "mailers"))
+      @choices << relative(File.join(@base, "test", "mailers", "%singular_name%_mailer_test.rb"))
+      
+      @assigns = {
+        :name                                   => name,  
+        :class_name                             => class_name, 
+        :class_path                             => class_path, 
+        :file_name                              => file_name, 
+        :class_nesting                          => class_nesting, 
+        :class_nesting_depth                    => class_nesting_depth, 
+        :plural_name                            => plural_name, 
+        :singular_name                          => singular_name,
+        :controller_name                        => controller_name,
+        :controller_class_path                  => controller_class_path,
+        :controller_file_path                   => controller_file_path,
+        :controller_class_nesting               => controller_class_nesting,
+        :controller_class_nesting_depth         => controller_class_nesting_depth,
+        :controller_class_name                  => controller_class_name,
+        :controller_singular_name               => controller_singular_name,
+        :controller_plural_name                 => controller_plural_name,
+        :model_controller_name                  => model_controller_name,
+        :model_controller_class_path            => model_controller_class_path,
+        :model_controller_file_path             => model_controller_file_path,
+        :model_controller_class_nesting         => model_controller_class_nesting,
+        :model_controller_class_nesting_depth   => model_controller_class_nesting_depth,
+        :model_controller_class_name            => model_controller_class_name,
+        :model_controller_singular_name         => model_controller_singular_name,
+        :model_controller_plural_name           => model_controller_plural_name,
+        :include_activation                     => include_activation
+      }
+
+      
+      m.directory File.join("test", "functional", controller_class_path)
+      m.directory File.join("test", "functional", model_controller_class_path)
+      
+      copy_dirs
+      copy_files
+      
     end
   end
 
