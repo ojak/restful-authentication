5e05fc0e8be5db58ead8b10429f9442733943301
Daniel Neighman <has.sox@gmail.com>
2008-03-07 00:49:52 +1100

    Adds sessions to routes

--- merb_generators/authenticated/templates/lib/authenticated_system/authenticated_routes.rb
+++ merb_generators/authenticated/templates/lib/authenticated_system/authenticated_routes.rb
@@ -5,8 +5,11 @@ module AuthenticatedSystem
       Merb::Router.prepend do |r|
         r.match("/login").to(:controller => "<%= controller_class_name %>", :action => "create").name(:login)
         r.match("/logout").to(:controller => "<%= controller_class_name %>", :action => "destroy").name(:logout)
+<% if include_activation -%>
         r.match("/<%= plural_name %>/activate/:activation_code").to(:controller => "<%= model_controller_class_name %>", :action => "activate").name(:<%= singular_name %>_activation)
-        r.resources :users
+<% end -%>
+        r.resources :<%= plural_name %>
+        r.resources :<%= controller_singular_name %>
       end
     end
   end
